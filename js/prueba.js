f7b25e3a9945c20e48c06cf6c8448e9e926135cd
Nicolas Sirtori <100817398+Robertsnights@users.noreply.github.com>
2023-03-27 19:22:18 -0300

    actualizacion

---
+++ js/prueba.js
@@ -0,0 +1,24 @@
+﻿let deferredPrompt;
+const butInstall = document.getElementsByClassName("descargar");
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    console.log("nice", "beforeinstallprompt", event);
+    window.deferredPrompt = event;
+    //divInstall.classList.toggle('hidden', false);
+});
+
+
+butInstall.addEventListener('click', async () => {
+    console.log("ok", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+    const result = await promptEvent.userChoise;
+    console.log("ok", "userChoise", result);
+    window.deferredPrompt = null;
+})
+
